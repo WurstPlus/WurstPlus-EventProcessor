673794ac3f1f1900e463cd819ab1e5f1ee52a5cf
Madmegsox1 <68086870+Madmegsox1@users.noreply.github.com>
2021-06-09 10:03:16 +0100

    [Improvement] improved iterations and overall speed of the EventProcessor

--- src/main/java/org/madmeg/wurstplus/event/EventProcessor.java
+++ src/main/java/org/madmeg/wurstplus/event/EventProcessor.java
@@ -1,8 +1,11 @@
 package org.madmeg.wurstplus.event;
 
 
+import org.jetbrains.annotations.NotNull;
+
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
@@ -11,7 +14,7 @@ import java.util.List;
  * @since 04/06/2021
  */
 
-public class EventProcessor {
+public final class EventProcessor {
 
     private final List<Listener> events;
 
@@ -23,28 +26,20 @@ public class EventProcessor {
      * @param object the class that the events are in
      * @throws IllegalArgumentException
      */
-    public void addEventListener(Object object) throws IllegalArgumentException {
+    public final void addEventListener(@NotNull Object object) throws IllegalArgumentException {
         getEvents(object);
     }
 
-    public void removeEventListener(Object object) {
-        List<Listener> toRemove = new ArrayList<>();
-        for(Listener listener : events){
-            if(object == listener.object){
-                toRemove.add(listener);
-            }
-        }
-        for (Listener listener : toRemove) {
-            events.remove(listener);
-        }
+    public final void removeEventListener(@NotNull Object object) {
+        events.removeIf(listener -> listener.object == object);
     }
 
     /**
      * @param object takes the class where the events are committed
      */
-    private void getEvents(Object object) {
+    private void getEvents(@NotNull Object object) {
         Class<?> clazz = object.getClass();
-        for (Method method : clazz.getDeclaredMethods()) {
+        Arrays.stream(clazz.getDeclaredMethods()).spliterator().forEachRemaining(method -> {
             if (method.isAnnotationPresent(CommitEvent.class)) {
                 Class<?>[] prams = method.getParameterTypes();
                 if (prams.length != 1) {
@@ -56,30 +51,28 @@ public class EventProcessor {
                 this.events.add(new Listener(method, object, prams[0], getPriority(method)));
                 this.events.sort(Comparator.comparing(o -> o.priority));
             }
-        }
+        });
     }
 
     /**
      * @param event the event that you are listening for
      * @return if the event was posted or not at a boolean
      */
-    public boolean postEvent(Event event) {
-
-        for (Listener listener : events){
+    public final boolean postEvent(@NotNull Event event) {
+        events.spliterator().forEachRemaining(listener -> {
             if(listener.event == event.getClass()){
                 try {
                     listener.method.setAccessible(true);
                     listener.method.invoke(listener.object, event);
                 } catch (Exception e) {
-                    System.out.println(e);
-                    return false;
+                    e.printStackTrace();
                 }
             }
-        }
+        });
         return true;
     }
 
-    private EventPriority getPriority(Method method) {
+    private EventPriority getPriority(@NotNull Method method) {
         return method.getAnnotation(CommitEvent.class).priority();
     }
 }
